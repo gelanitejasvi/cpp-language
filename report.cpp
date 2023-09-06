a4cd59dcb39b0afe5bbb75b5437b4081ff82891d
tejasvi <tejasvigelani@gmail.com>
2023-09-06 12:00:15 +0530

    cpp basic

--- report.cpp
+++ report.cpp
@@ -15,6 +15,31 @@ class report
             cin>>arr[i];
             total+=arr[i];
         }
-        
+        return total/5;        
     }
+    public:
+    void readinfo()
+    {
+        cout<<"admin no:";
+        cin>>adno;
+        cout<<"student name:";
+        cin>>name;
+        average=getavg();
+    }
+
+    void displayinfo()
+    {
+        cout<<"admin no:"<<adno<<endl;
+        cout<<"student name:"<<name<<endl;
+        cout<<"marks are:"<<arr[0]<<" "<<arr[1]<<" "<<arr[2]<<" "<<arr[3]<<" "<<arr[4]<<endl;
+        cout<<"average:"<<average<<endl;\
+
+    }
+};
+
+int main()
+{
+    report c;
+    c.readinfo();
+    c.displayinfo();
 }
